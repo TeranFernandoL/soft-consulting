da552a03f6396394871607b3ac59afbebb2b8026
Carranza Jhonatan <jhonatan.carranza@unmsm.edu.pe>
2020-07-23 02:17:20 -0500

    Vista Home
    Vista home
    csshome

--- Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/home.js
+++ Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/home.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '../public/csshome/cssjh.css';
+
 
 export default function Home() {
-    return 'Home...';
+    return (
+
+        <>
+        <br></br>
+        <div className=" container contenido  pt-5"  id="" align="center">
+            <div className="">
+                <div className="container  titulos1" >
+                    <h1 >BIENVENIDO A TIRS</h1>
+                    <h5>Tu Herramienta de Identificación de Requisitos de Software</h5> 
+                </div>
+                <br></br>
+                <div className="container  p-5 ">
+                    <div   className="form-group row">
+                        <div className="col-sm-6">
+                             <img className="imagen" src="img2.jpg" alt="image" width="450" height="400"></img>
+                        </div>
+                        <div className="col-sm-6 pt-5">
+                            <br></br><br></br><br></br><br></br>
+                            <a href="/login" className="btn btn-primary btn-block text-uppercase font-weight-bold mb-2" type="">Acceder</a><br></br>
+                            <a href="/registro" className="btn btn-secondary btn-block text-uppercase font-weight-bold mb-2" type="s">Crear cuenta</a>
+                            <br></br>
+                            <img  src="redux.png" width="40" height="40" ></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </>
+    );
 };
