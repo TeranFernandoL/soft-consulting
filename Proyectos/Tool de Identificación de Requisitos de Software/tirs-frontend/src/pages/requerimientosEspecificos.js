90c19ffd83ac182f3759c5e59fddc8d55cbc37cc
Mary Luz <mary.quispe1@unmsm.edu.pe>
2020-08-07 20:03:14 -0500

    Fix: TablaRequerimientoEspecifico
    TablaRequerimientoEspecifico

--- Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/requerimientosEspecificos.js
+++ Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/requerimientosEspecificos.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import {Container} from 'react-bootstrap';
+import useFetch from '../hooks/useFetch';
+import {getJsonStorage} from '../lib/jsonStorages'
 
+import TablaRequerimientosEspecificos from '../components/RequerimientosEspecificos/tablaRequerimientosEspecificos'
+console.log(TablaRequerimientosEspecificos);
 export default function RequerimientosEspecificos() {
+    
     return (
         <Container>
         <br></br><br></br>
         <h3 style={{textAlign: "center"}}>"Proyecto" - Requerimientos Especificos</h3>
-        <h6>Boton Agregar</h6>
-        <h6>Componente: Tabla con los requerimientos listados (modificar, eliminar) - La tabla será estática por el momento</h6>
+        <br></br>
+        <>
+            <button type="button" className="btn btn-success" >Nuevo Requerimiento</button>
+            <br /><br />
+            
+            <TablaRequerimientosEspecificos/>
+        </>
         </Container>
     )
 };
