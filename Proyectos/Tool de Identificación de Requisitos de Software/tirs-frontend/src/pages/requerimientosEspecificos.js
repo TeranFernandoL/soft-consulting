9f1de8c1cc9291afec0bed00ab8684729105089c
Mary Luz <mary.quispe1@unmsm.edu.pe>
2020-08-21 14:30:08 -0500

    Fix:Requerimientos especificos
    Fix:Requerimientos especificos

--- Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/requerimientosEspecificos.js
+++ Proyectos/Tool de Identificación de Requisitos de Software/tirs-frontend/src/pages/requerimientosEspecificos.js
@@ -8,8 +8,32 @@ console.log(TablaRequerimientosEspecificos);
 export default function RequerimientosEspecificos() {
     
     return (
+        
         <Container>
+            
+        {/* <nav id="menu">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Quienes somos</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Servicios</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Garantías</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Clientes</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Ofertas</a></li>
+            <li class="barra">|</li>
+            <li><a href="mantenimientos/productos.html">Productos</a></li>
+            <li class="barra">|</li>
+            <li><a href="mantenimientos/categorias.html">Categorias</a></li>
+            <li class="barra">|</li>
+            <li><a href="#">Ubícanos</a></li>
+        </ul>
+    </nav> */}
         <br></br><br></br>
+        
         <h3 style={{textAlign: "center"}}>"Proyecto" - Requerimientos Especificos</h3>
         <br></br>
         <>
